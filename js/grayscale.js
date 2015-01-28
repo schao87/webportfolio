e0ecbe6a4d1ea24ed7530b0e6899a23ee686ff37
schao87 <schaosti@gmail.com>
2015-01-27 22:36:26 -0500

    added gun game

--- js/grayscale.js
+++ js/grayscale.js
@@ -3,83 +3,106 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-
+$(document).ready(function(){
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        $("a.page-scroll").hover(
-            function(){$(this).css("background", "rgba(255,255,255,.3)")},
-            function(){$(this).css("background", "none")}
-        );
-    }
-});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $("a.page-scroll").hover(
+                function(){$(this).css("background", "rgba(255,255,255,.3)")},
+                function(){$(this).css("background", "none")}
+            );
+        }
     });
-});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 
 
-$( '.thumbnails img' ).on("mouseover", function() {
-        console.log('tween');
-        TweenMax.to($(this), .8, {height:"106px", width:"306px", top:"-3px", ease:Elastic.easeOut});
-        // TweenMax.to($(this), 0, {margin:"3px"});
-});
-$( '.thumbnails img' ).on("mouseleave", function() {
-        console.log('tween');
-        TweenMax.to($(this), .3, {height:"100px", width:"300px", top:"0px"});
-        // TweenMax.to($(this), 0, {margin:"6px"});
-});
-$('.callTrain').on('mousedown', function(){
-    $(this).css('background', 'yellow');
-});
-$('.callTrain').on("mouseup", function(){
-    TweenMax.to($('.train'), 2.7, {delay:1, left:"0px", ease:Expo.easeOut});
-    $(this).css('background', 'red');
-    buttonHide();
-});
-var doorOpened = false;
-var departing;
+    $( '.thumbnails img' ).on("mouseover", function() {
+            console.log('tween');
+            TweenMax.to($(this), .8, {height:"106px", width:"306px", top:"-3px", ease:Elastic.easeOut});
+            // TweenMax.to($(this), 0, {margin:"3px"});
+    });
+    $( '.thumbnails img' ).on("mouseleave", function() {
+            console.log('tween');
+            TweenMax.to($(this), .3, {height:"100px", width:"300px", top:"0px"});
+            // TweenMax.to($(this), 0, {margin:"6px"});
+    });
+    $('.callTrain').on('mousedown', function(){
+        $(this).css('background', 'yellow');
+    });
+    $('.callTrain').on("mouseup", function(){
+        TweenMax.to($('.train'), 3, {delay:1, left:"0px", ease:Expo.easeOut});
+        $(this).css('background', 'red');
+        buttonHide();
+    });
+    var doorOpened = false;
+    var departing;
 
-function reset(){
-    $('.train').css("left", "1290px");
-    $('.callTrain').css('visibility', 'visible');
-}
-function buttonHide(){
-    $('.callTrain').css('visibility', 'hidden');
-}
-function depart(){
-    if(doorOpened == false){
-        departing = TweenMax.to($('.train'), 2, {delay:.3, left:"-2000px", ease:Expo.easeIn, onComplete:reset});
+    function reset(){
+        $('.train').css("left", "1290px");
+        $('.callTrain').css('visibility', 'visible');
     }
-}
-$('.doorway').on("click", function(){
-    if (doorOpened == false){
-        TweenMax.to($('.leftdoor'), 1.5, {left:"-75px", ease:Linear.easeIn});
-        TweenMax.to($('.rightdoor'), 1.5, {right:"-75px", ease:Linear.easeIn});
-        doorOpened = true;
-        departing.kill();//stops depart function
-        console.log("door opened");
-    }else{
-        doorOpened = false;
-        TweenMax.to($('.leftdoor'), 1.5, {left:"0px", ease:Linear.easeIn, onComplete:depart});
-        TweenMax.to($('.rightdoor'), 1.5, {right:"0px", ease:Linear.easeIn});
-        console.log("door closed"); 
+    function buttonHide(){
+        $('.callTrain').css('visibility', 'hidden');
     }
+    function depart(){
+        if(doorOpened == false){
+            departing = TweenMax.to($('.train'), 2, {delay:.3, left:"-2000px", ease:Expo.easeIn, onComplete:reset});
+        }
+    }
+    $('.doorway').on("click", function(){
+        if (doorOpened == false){
+            TweenMax.to($('.leftdoor'), 1.5, {left:"-75px", ease:Linear.easeIn});
+            TweenMax.to($('.rightdoor'), 1.5, {right:"-75px", ease:Linear.easeIn});
+            doorOpened = true;
+            departing.kill();//stops depart function
+            console.log("door opened");
+        }else{
+            doorOpened = false;
+            TweenMax.to($('.leftdoor'), 1.5, {left:"0px", ease:Linear.easeIn, onComplete:depart});
+            TweenMax.to($('.rightdoor'), 1.5, {right:"0px", ease:Linear.easeIn});
+            console.log("door closed"); 
+        }
+    });
+    var targetPull;
+    var targetReset;
+
+    function targetReset(){
+        TweenMax.to($('.target'), 0, {left:"-40px", top:"60px"});
+    };
+    function targetPull(){
+        TweenMax.to($('.target'), 3.5, {left:"600px", ease:Linear.easeNone});
+    };
+    
+    $('.start').on('click', function(){
+        targetReset();
+        targetPull();
+        $('.target').on('click', function(){
+            TweenMax.to($('.target'), 1, {top:"300px", ease:Linear.easeOut, onComplete:targetReset});
+        });
+    });
+    
+
+
+
+
 });
 
 
