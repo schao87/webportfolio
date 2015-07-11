552e8856c1466754941426a6a61c502eb55a1dfa
schao87 <schaosti@gmail.com>
2015-07-11 13:40:16 -0400

    fixed optimus

--- js/grayscale.js
+++ js/grayscale.js
@@ -371,9 +371,9 @@ $(document).ready(function(){
         vehicle = false;
     }
     var prime = new TimelineMax();
-        prime.to($(".forearm"), .4, {marginTop:'-35px', delay: .5, ease: Bounce.easeOut})
+        prime.to($(".forearm"), .4, {marginTop:'-35px', delay: 1, ease: Bounce.easeOut})
             .to($('.hands'), .4, {marginTop:'-35px', ease: Bounce.easeOut})
-            .to($('.abs'), .4, {borderLeft: '0', borderRight: '0', width: '158px', ease: Bounce.easeOut}, 'widenUp')
+            .to($('.abs'), .4, {borderLeft: '0px', borderRight: '0px', width: '158px', ease: Bounce.easeOut}, 'widenUp')
             .to($('.redAbs'), .41, {width: '158px', ease: Bounce.easeOut}, 'widenUp')
             .to($('.pelvis'), .4, {width: '160px', ease: Bounce.easeOut}, 'widenUp')
             .to($('.reflectors'), 0, {display: 'none'}, 'widenUp')
@@ -384,18 +384,16 @@ $(document).ready(function(){
             .to($('.arms'), .4, {marginTop: '75px', ease: Bounce.easeOut}, 'dropDown')
             .to($('.head'), .4, {marginTop: '145px', ease: Bounce.easeOut}, 'dropDown')
             .to($('.torsoTop'), .4, {marginTop: '242px', ease: Bounce.easeOut}, 'dropDown2')
-            .to($('.torsoTop'), .4, {marginTop: '242px', ease: Bounce.easeOut}, 'dropDown2')
-            .to($('.arms'), .4, {marginTop: '242px', ease: Bounce.easeOut}, 'dropDown2')
+            .to($('.arms'), .22, {marginTop: '242px', ease: Power2.easeOut}, 'dropDown2')
             .to($('.head'), .4, {marginTop: '316px', ease: Bounce.easeOut}, 'dropDown2')
             .to($('.thighs'), .4, {marginTop: '74px', ease: Bounce.easeOut}, 'dropDown2')
             .to($('.leg'), 0, {display: 'none'})
             .to($('.tires'), .2, {display: "block"})
             .to($('.torsoTop'), .4, {marginTop: '226px', ease: Bounce.easeOut}, 'bounceUp')
-            .to($('.torsoTop'), .4, {marginTop: '226px', ease: Bounce.easeOut}, 'bounceUp')
-            .to($('.arms'), .4, {marginTop: '226px', ease: Bounce.easeOut}, 'bounceUp')
+            .to($('.arms'), .41, {marginTop: '226px', ease: Bounce.easeOut}, 'bounceUp')
             .to($('.head'), .4, {marginTop: '300px', ease: Bounce.easeOut}, 'bounceUp')
             .to($('.thighs'), .4, {marginTop: '58px', ease: Bounce.easeOut}, 'bounceUp')
-            .to($('.lights'), .4, {opacity: '1', onComplete:truck});
+            .to($('.lights'), .4, {opacity: '1'});
     
     
     $('.wrap').on('click', function(){
